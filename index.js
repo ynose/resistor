3731f9ce0e591aaba579f6d94a3f4cc03a775a36
Yoshio Nose <ynose.249@gmail.com>
2015-02-04 23:26:12 +0900

    抵抗値の算出機能を追加

--- index.js
+++ index.js
@@ -13,6 +13,66 @@ $(document).ready(function(){
         // 帯の値を表示
         var ohmNo = $(this).data("ohm-no");
         $(this).closest(".js-ohm-group").find(".js-ohm-value").text((ohmNo == undefined ? "" : ohmNo));
+        
+        // 抵抗値を表示
+        var value_1 = $("#ohm-value-1").text();
+        var value_2 = $("#ohm-value-2").text();
+        var value_3 = $("#ohm-value-3").text();
+        var magnification;
+        var ohm;
+        switch (value_3) {
+            case "0":
+              magnification = 1;
+              break;
+            case "1":
+              magnification = 10;
+              break;
+            case "2":
+              magnification = 100;
+              break;
+            case "3":
+              magnification = 1000;
+              break;
+            case "4":
+              magnification = 10000;
+              break;
+            case "5":
+              magnification = 100000;
+              break;
+            case "6":
+              magnification = 1000000;
+              break;
+            case "G":
+              magnification = 0.1;
+              break;
+            case "S":
+              magnification = 0.01;
+              break;
+            default:
+              magnification = 0;
+              break;
+        }
+        ohm = Number(value_1 + value_2) * magnification;
+        
+        switch (magnification) {
+            case 0.1:
+            case 0.01:
+                $(".caption").text(ohm + " Ω");
+                break;
+            case 100:
+            case 1000:
+            case 10000:
+                $(".caption").text((ohm / 1000) + "K Ω");
+                break;
+            case 100000:
+            case 1000000:
+                $(".caption").text((ohm / 1000000) + "M Ω");
+                break;
+            default:
+                $(".caption").text(ohm + " Ω");
+                break;
+        }
+        
     });
 
 });
